40c7f1e89764713582429459ddceb4e5d77412f4
Zhenjian Zhang <itdesk.zhang@gmail.com>
2020-06-02 17:57:10 +0800

    更新rely_on
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- openwrt.sh
+++ openwrt.sh
@@ -53,7 +53,7 @@ source_make_clean() {
 }
 
 rely_on() {
-	sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf bison g++ gcc help2man htop ncurses-term ocaml-nox sharutils yui-compressor make cmake 
+	sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget bison g++ gcc help2man htop ncurses-term ocaml-nox sharutils yui-compressor make cmake
 }
 
 #显示编译文件夹
