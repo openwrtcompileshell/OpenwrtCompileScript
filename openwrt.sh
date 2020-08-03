bf201a0cca85dd19241afa9f387021ed645b06c2
Zhenjian Zhang <itdesk.zhang@gmail.com>
2020-08-03 21:33:39 +0800

    other-plugins增加adguardhome插件
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- openwrt.sh
+++ openwrt.sh
@@ -1502,6 +1502,14 @@ COMMENT
 			git clone https://github.com/jerrykuku/node-request.git package/other-plugins/node-request
 			git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/other-plugins/luci-app-jd-dailybonus
 		fi
+
+		#adguardhome插件
+		if [[ -e package/other-plugins/luci-app-adguardhome ]]; then
+			cd  package/luci-app-adguardhome && source_update_No_git_pull
+			cd $HOME/$OW/$file/lede/
+		else
+			https://github.com/rufengsuixing/luci-app-adguardhome.git package/other-plugins/luci-app-adguardhome
+		fi
 :<<'COMMENT'
 		#取消IPV6
 		ipv6=$(grep "+IPV6:libip6tc" package/network/config/firewall/Makefile | wc -l)
