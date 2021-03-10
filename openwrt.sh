f2097cb4e217a57f67db02e480f07be4ebd204c2
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-03-10 15:09:35 +0800

    调整脚本到最新
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- openwrt.sh
+++ openwrt.sh
@@ -2042,6 +2042,8 @@ noclean_make() {
 
 update_clean_make() {
 	clear
+	echo -e "$green>>更新脚本到最新$white"
+	update_script
 	echo -e "$green>>文件夹:$action1 执行make clean$white"
 		make clean && rm -rf .config && rm -rf ./tmp/ && rm -rf ./feeds
 	echo -e "$green>>文件夹:$action1 执行git pull$white"
