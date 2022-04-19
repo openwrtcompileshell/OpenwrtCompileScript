5f4ab1c2ebef44bfb5258cd0dd7b292a78879e6a
ITdesk <>
2022-04-19 20:23:58 +0800

    简单更新
    Signed-off-by: ITdesk <>

--- package/luci-app-banlogon/root/usr/share/banlogon/banlogon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ipset list uhttpd4 2>/dev/null|grep timeout|sed '/Header:/d'|awk '{print "uhttpd",$1,$3}'
-ipset list uhttpd6 2>/dev/null|grep timeout|sed '/Header:/d'|awk '{print "uhttpd",$1,$3}'
-ipset list dropbear4 2>/dev/null|grep timeout|sed '/Header:/d'|awk '{print "dropbear",$1,$3}'
-ipset list dropbear6 2>/dev/null|grep timeout|sed '/Header:/d'|awk '{print "dropbear",$1,$3}'
